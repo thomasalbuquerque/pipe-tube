21e424b455066f9f8011d6e104348774e7b8303e
Thomas Albuquerque <tealbthomas@gmail.com>
2023-07-03 12:18:23 -0300

    Feito o Header, criadas imagens do Header, instalação prettier-plugin-tailwind.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,40 +3,48 @@ import React from 'react';
 const Header = () => {
   return (
     <>
-      <div className="flex items-center justify-between py-1 shadow-md w-screen">
-        <div className="flex items-center">
+      <div className="flex w-full items-center justify-between py-1 shadow-md">
+        <div className="m-2 flex flex-col-reverse gap-2 xsm:flex-row xsm:items-center sm:m-4 sm:flex-nowrap sm:gap-4">
           <img
-            className="w-5 h-4 m-4 md:m-6"
-            src="/hamburger-icon-2.png"
+            className="w-4 pl-1 sm:w-5"
+            src="/hamburger-icon.png"
             alt="hamburger menu icon"
             id="openNav"></img>
           <img
-            className="w-24 md:w-48" // Adicione uma classe de responsividade para o tamanho do logo
-            src="/pipetube-logo-v4.png"
+            className="w-16 px-1 xsm:px-2 sm:w-24"
+            src="/pipetube-logo.png"
             alt="website logo pipe tube"
             id="logo"></img>
         </div>
         <div
           id="searchBar"
-          className="flex items-center md:justify-between border border-lightGray border-solid bg-white rounded-2xl py-1 px-4 min-w-0 h-10">
-          <form action="/send-data-here" method="post" className=" min-w-0">
+          className="flex h-7 w-2/4 items-center rounded-lg border border-solid border-lightGray bg-white px-4 py-1 xsm:h-9 md:justify-between md:rounded-xl xl:w-1/4">
+          <form action="/send-data-here" method="post" className="w-full">
             <input
-              className="focus:outline-none text-grayray max-w-full"
+              className="text-grayray h-full w-full focus:outline-none"
               type="text"
               placeholder="Search"
             />
           </form>
           <img
-            className="w-4 h-4 md:mx-4"
-            src="/magnifier-glass-3.png"
+            className="h-3 w-3 xsm:h-4 xsm:w-4 md:mx-1"
+            src="/magnifier-glass.png"
             alt=""
           />
         </div>
         <div
           id="rightButtons"
-          className="flex items-center justify-between mr-6">
-          <img className="w-5 h-5 ml-4" src="/upload-icon-2.png" alt="" />
-          <img className="w-10 h-10 ml-4" src="/profile-pic.png" alt="" />
+          className="mr-6 flex items-center justify-between">
+          <img
+            className="ml-4 h-3 w-3 xsm:h-5 xsm:w-5"
+            src="/upload-icon.png"
+            alt=""
+          />
+          <img
+            className="ml-4 h-7 w-7 xsm:h-10 xsm:w-10"
+            src="/profile-pic.png"
+            alt=""
+          />
         </div>
       </div>
     </>
