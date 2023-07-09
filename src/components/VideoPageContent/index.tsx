db5ca0278bb201c35570b1f59f027ac669a7f4aa
Thomas Albuquerque <tealbthomas@gmail.com>
2023-07-09 15:38:14 -0300

    Feita tela de vídeo, seção de detalhes (título, descrição, nome do canal, botões etc.)

--- src/components/VideoPageContent/index.tsx
+++ src/components/VideoPageContent/index.tsx
@@ -4,53 +4,92 @@ import VideoPlayer from '../VideoPlayer';
 import videos_data from '@/utils/videos_data.json';
 import { formatDate, formatViewCount, shuffleArray } from '@/helpers/functions';
 import CardVideo from '../CardVideo';
+import { AiOutlineLike } from 'react-icons/ai';
+
 interface props {
   menuOpen: boolean;
   videoId: string;
 }
 const VideoPageContent = ({ menuOpen, videoId }: props) => {
-  const [videos, setVideos] = useState<Video[]>(
-    shuffleArray(JSON.parse(JSON.stringify(videos_data)), 12)
-  );
+  const [allVideos, setVideos] = useState<Video[]>([]);
+  const [recommendedVideos, setRecommendedVideos] = useState<Video[]>([]);
   const [currentVideo, setcurrentVideo] = useState<Video>();
+
   useEffect(() => {
-    setcurrentVideo(videos.find((video: Video) => video.videoId === videoId));
-  }, [videos]);
+    setVideos(shuffleArray(JSON.parse(JSON.stringify(videos_data)), 50));
+  }, []);
+
+  useEffect(() => {
+    if (!allVideos) {
+      return;
+    } else {
+      setcurrentVideo(
+        allVideos.find((video: Video) => video.videoId == videoId)
+      );
+    }
+  }, [allVideos]);
+
+  useEffect(() => {
+    if (!currentVideo) return;
+
+    setRecommendedVideos(allVideos.slice(0, 12));
+  }, [currentVideo]);
+  if (!currentVideo) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
       <div
         className={clsx(
-          'absolute flex flex-wrap items-start justify-center gap-10 pr-4 duration-200 xsm:p-5',
+          'absolute flex flex-wrap items-start gap-10 pr-4 duration-200 xsm:p-5',
           menuOpen && `left-32 xsm:left-40`,
           !menuOpen && `left-12 xsm:left-16`
         )}>
-        <div className="flex w-[80rem] flex-col items-start">
-          <VideoPlayer videoId={videoId} />
-          <div>{currentVideo ? currentVideo.videoTitle : ''}</div>
-          <div className="flex">
-            <div>
-              Youtube views:{' '}
-              {currentVideo ? formatViewCount(currentVideo.viewCount) : ''} |{' '}
-              {currentVideo ? formatDate(currentVideo.uploadDate) : ''}
+        <div>
+          <div className="flex w-[80rem] flex-col items-start">
+            <VideoPlayer videoId={videoId} />
+            <h1 className="mt-4 text-xl">{currentVideo.videoTitle}</h1>
+            <div className="mt-3 flex items-center gap-5">
+              <div className="flex items-center gap-2">
+                <img src="/channel-profile-pic.png" alt="" />
+                <div className="font-medium">{currentVideo.channelName}</div>
+              </div>
+              <button className="h-8 w-24 rounded-md border border-customGray bg-accent font-medium text-background">
+                Subscribe
+              </button>
+              <button className="flex h-8 w-24 items-center justify-center gap-1 rounded-md border border-customGray bg-accent font-medium text-background">
+                <span>Like</span>
+                <span>
+                  <AiOutlineLike />{' '}
+                </span>
+              </button>
             </div>
-          </div>
-          <button>Like</button>
-          <div>{currentVideo ? currentVideo.channelName : ''}</div>
-          <div className="">Description</div>
-          <div>
-            {currentVideo
-              ? currentVideo.description.split('\n').map((line, index) => (
+
+            <div className="mt-3 h-fit w-[80rem] rounded-md bg-lightOrange">
+              <div className="m-3 mb-4 flex items-center justify-between">
+                <div className="text-sm font-medium">Description</div>
+                <div className="text-sm font-medium">
+                  {formatDate(currentVideo.uploadDate)} |{' '}
+                  {formatViewCount(currentVideo.viewCount)}
+                  {'  '}
+                  views
+                </div>
+              </div>
+              <div className="m-3 text-sm font-normal">
+                {currentVideo.description.split('\n').map((line, index) => (
                   <React.Fragment key={index}>
                     {line}
                     <br />
                   </React.Fragment>
-                ))
-              : ''}
+                ))}
+              </div>
+            </div>
+
+            {/*  */}
           </div>
         </div>
-
         <div className="flex h-[50rem] w-96 flex-col items-center gap-5">
-          {videos.map((video: Video) => (
+          {recommendedVideos.map((video: Video) => (
             <CardVideo key={video.videoId} video={video} />
           ))}
         </div>
