5d7da01a1a30b4418fc409558ea277af379c984f
Thomas Albuquerque <tealbthomas@gmail.com>
2023-07-14 17:26:28 -0300

    Criada página Help

---
+++ src/app/help/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React from 'react';
+import Header from '@/components/Header';
+import HomeContent from '@/components/HomeContent';
+import NavBar from '@/components/NavBar';
+import ToastNote from '@/components/ToastNote';
+import NoteMessage from '@/components/NoteMessage';
+
+const Page = () => {
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  return (
+    <>
+      <main className="bg-lightOrangeBG">
+        <div className="h-20">
+          <Header />
+        </div>
+        <div className="">
+          <div className="">
+            <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          </div>
+          <div className="relative">
+            <div className="absolute left-32 top-12 text-customGray">
+              <h3 className="mx-6 text-3xl font-semibold">Note</h3>
+              <NoteMessage />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Page;
